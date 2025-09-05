66de4283bf196f62b67b2eea51bbdf8fa122c7fa
Alexander Rahemat <118708187+Dewm-Bot@users.noreply.github.com>
2025-09-05 10:38:18 -0400

    home.nix temp

---
+++ home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+	let
+  	home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+	in
+{
+	imports =
+	[
+		(import "${home-manager}/nixos")
+		./packages/home-pkgs.nix
+	];
+
+	home-manager.users.dewm = { pkgs, ... }: {
+		home.packages = [ pkgs.atool pkgs.httpie ];
+		programs.bash.enable = true;
+  
+		# The state version is required and should stay at the version you
+		# originally installed.
+		home.stateVersion = "25.05";
+	};
+}
