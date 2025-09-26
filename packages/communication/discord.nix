b1f4718d23a2ae3ffdc89d56b3fa33ac65ffac76
Alexander Rahemat <118708187+Dewm-Bot@users.noreply.github.com>
2025-09-26 12:20:55 -0400

    Declaration update

--- packages/communication/discord.nix
+++ packages/communication/discord.nix
@@ -2,7 +2,7 @@
 
 {
     environment.systemPackages = with pkgs; [
-        discord
+        #discord
         equicord
         equibop
         discordo
--- packages/media/apps.nix
+++ packages/media/apps.nix
@@ -1,36 +1,51 @@
 { config, pkgs, inputs, ... }:
 
-{
-    environment.systemPackages = with pkgs; [
-        feishin
-        mpv
-        mpvpaper
-        mpvScripts.mpris
-        mpvScripts.modernx
-        #mpvScripts.autosub
-        mpvScripts.videoclip
-        obs-studio
-        obs-studio-plugins.obs-3d-effect
-        obs-studio-plugins.wlrobs
-        obs-studio-plugins.waveform
-        obs-studio-plugins.obs-tuna
-        obs-studio-plugins.obs-webkitgtk
-        obs-studio-plugins.obs-vkcapture
-        obs-studio-plugins.input-overlay
-        mpdris2
-        mpd
-        mpd-notification
-        cantata
-        inori
-        ashuffle
-        rofi-mpd
-        yt-dlp
-        radiotray-ng
-        ncmpcpp
-        rmpc
-        kitty-img
+let
+  mpvScripts = [  #Define scripts for MPV here
+    pkgs.mpvScripts.mpris
+    pkgs.mpvScripts.modernx
+    pkgs.mpvScripts.videoclip
+  ];
+
+
+in {
+  nixpkgs.overlays = [
+    (self: super: {
+      mpv = super.mpv.override {
+        scripts = mpvScripts; #Apply scripts overlay
+      };
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [
+    feishin
+    mpv
+    mpvpaper
+    mpdris2
+    mpd
+    mpd-notification
+    cantata
+    inori
+    ashuffle
+    rofi-mpd
+    yt-dlp
+    radiotray-ng
+    ncmpcpp
+    rmpc
+    kitty-img
+
+  ];
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs; [
+      obs-studio-plugins.obs-3d-effect
+      obs-studio-plugins.wlrobs
+      obs-studio-plugins.waveform
+      obs-studio-plugins.obs-tuna
+      obs-studio-plugins.obs-webkitgtk
+      obs-studio-plugins.obs-vkcapture
+      obs-studio-plugins.input-overlay
     ];
-
+  };
 }
-
-
