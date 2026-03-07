d3b806ce31a0e1bf5dfc655895a5200485851433
Alexander Rahemat <118708187+Dewm-Bot@users.noreply.github.com>
2026-03-07 01:17:09 -0500

    TuneD Settings

--- laptop-rog.nix
+++ laptop-rog.nix
@@ -15,8 +15,34 @@
         };
     };
 
-    #hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    boot.blacklistedKernelModules = [
+        "nova"
+        "nova_core"
+        "nova_drm"
+    ];
 
+    services.tlp.enable = false;
+
+    services.tuned.enable = true;
+    services.tuned.settings.daemon = true;
+    services.tuned.ppdSupport = true;
+    services.tuned.settings.profile_dirs = "/etc/tuned/";
+    services.tuned.ppdSettings = {
+        profiles = {
+            balanced = "balanced-bazzite";
+            performance = "throughput-performance-bazzite";
+            power-saver = "powersave-bazzite";
+        };
+        battery = {
+            balanced = "balanced-battery-bazzite";
+            performance = "balanced-bazzite";
+            power-saver = "powersave-battery-bazzite";
+        };
+        main = {
+            default = "balanced";
+            battery_detection = true;
+        };
+    };
 }
 
 
